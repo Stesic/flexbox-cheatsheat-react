f49995e343ea2cfc17667e78ccb4f1d3c9ff07b5
Ivan Stesic <ivan.stesic@gmail.com>
2019-01-17 21:55:22 +0100

    bug fix

--- src/view/components/Header/Header.js
+++ src/view/components/Header/Header.js
@@ -4,10 +4,20 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
+    const state = {
+        homePage: true
+    }
+    if(window.location.pathname === '/'){
+        state.homePage = true;
+    }else {
+        state.homePage = false;
+    }
     return (
         <header className='capitalizeText'>
             <h1><Link to='/'>flexbox properties</Link></h1>
-            <p>Simple app where user can see flexbox properties and how they can be used</p>
+            {state.homePage? 
+            <p>Simple app where user can see flexbox properties and how they can be used</p> : ""
+            }
         </header>
     )
 }
